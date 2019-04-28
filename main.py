27c430a2250a7c10bdd90621c879831c6b529b4d
Dongsik <dongsiku+git@gmail.com>
2019-04-29 08:40:39 +0900

    Add the feature for sha512

--- main.py
+++ main.py
@@ -9,28 +9,46 @@ Created on Tue Aug  7 22:00:38 2018
 
 import hashlib
 import sys
+from os import path
 
 
-def return_hash_from_iso(iso_filename):
-    with open(iso_filename, 'rb') as f:
-        checksum = hashlib.sha256(f.read()).hexdigest()
-        print("Hash value: {}".format(checksum))
+class VerifySHA:
+    def __init__(self, mainfilename):
+        self.mainfilename = mainfilename
 
-    return checksum
+    def return_hash_from_mainfile(self, sha_num):
+        with open(self.mainfilename, 'rb') as f:
+            if sha_num == 256:
+                checksum = hashlib.sha256(f.read()).hexdigest()
+            elif sha_num == 512:
+                checksum = hashlib.sha512(f.read()).hexdigest()
+            print("Hash value: {}".format(checksum))
+
+        return checksum
 
 
-def check_hash_key(iso_filename):
-    sha256_filename = "{}.sha256".format(iso_filename.rsplit(".", 1)[0])
-
-    with open(sha256_filename, "r") as f:
-        hash_value = return_hash_from_iso(iso_filename)
-        sha256_file = f.readline().strip()
-        print(".sha256: {}".format(sha256_file))
-        if sha256_file == hash_value:
-            print("OK")
+    def check_hash_key(self):
+        sha256_filename = "{}.sha256".format(self.mainfilename.rsplit(".", 1)[0])
+        sha512_filename = "{}.sha512".format(self.mainfilename.rsplit(".", 1)[0])
+        if path.isfile(sha256_filename):
+            sha_filename = sha256_filename
+            sha_num = 256
+        elif path.isfile(sha512_filename):
+            sha_filename = sha512_filename
+            sha_num = 512
         else:
-            print("Error")
+            raise FileNotFoundError("No file *.sha256 or *.sh512")
+
+        with open(sha_filename, "r") as f:
+            hash_value = self.return_hash_from_mainfile(sha_num)
+            sha_file = f.readline().strip()
+            print(".sha{}: {}".format(sha_num, sha_file))
+            if sha_file == hash_value:
+                print("OK")
+            else:
+                print("Error")
 
 
 if __name__ == "__main__":
-    check_hash_key(sys.argv[1])
+    vsha = VerifySHA(sys.argv[1])
+    vsha.check_hash_key()
