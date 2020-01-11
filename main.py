52ea14dcfecfd1b3180322280a345f7d76637b0f
Dongsik <dongsiku+git@gmail.com>
2020-01-12 08:47:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,14 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Aug  7 22:00:38 2018
-
-@author: crantu
-"""
-
-
 import hashlib
 import sys
 from os import path
 
 
 class VerifySHA:
-    def __init__(self, mainfilename):
-        self.mainfilename = mainfilename
+    def __init__(self, target_filename):
+        self.target_filename = target_filename
 
-    def return_hash_from_mainfile(self, sha_num):
-        with open(self.mainfilename, 'rb') as f:
+    def return_hash_from_target_file(self, sha_num):
+        with open(self.target_filename, 'rb') as f:
             if sha_num == 256:
                 checksum = hashlib.sha256(f.read()).hexdigest()
             elif sha_num == 512:
@@ -26,10 +17,9 @@ class VerifySHA:
 
         return checksum
 
-
     def check_hash_key(self):
-        sha256_filename = "{}.sha256".format(self.mainfilename.rsplit(".", 1)[0])
-        sha512_filename = "{}.sha512".format(self.mainfilename.rsplit(".", 1)[0])
+        sha256_filename = "{}.sha256".format(self.target_filename)
+        sha512_filename = "{}.sha512".format(self.target_filename)
         if path.isfile(sha256_filename):
             sha_filename = sha256_filename
             sha_num = 256
@@ -39,11 +29,20 @@ class VerifySHA:
         else:
             raise FileNotFoundError("No file *.sha256 or *.sh512")
 
-        with open(sha_filename, "r") as f:
-            hash_value = self.return_hash_from_mainfile(sha_num)
-            sha_file = f.readline().strip().split(" ", 1)[0]
-            print(".sha{}: {}".format(sha_num, sha_file))
-            if sha_file == hash_value:
+        with open(sha_filename, "r") as sha_f:
+            hash_value = self.return_hash_from_target_file(sha_num)
+            sha_lines = sha_f.readlines()
+            for sha_line in sha_lines:
+                temp_sha_line = sha_line.strip().rsplit(" ", 1)
+                if len(temp_sha_line) > 1:
+                    if temp_sha_line[1] == self.target_filename:
+                        saved_hase_value = temp_sha_line[0]
+                        break
+                else:
+                    saved_hase_value = temp_sha_line[0]
+
+            print(".sha{}: {}".format(sha_num, saved_hase_value))
+            if saved_hase_value == hash_value:
                 print("OK")
             else:
                 print("Error")
