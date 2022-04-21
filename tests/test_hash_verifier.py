3f3d97d1327bd5f31f19515af568844d11439cb9
Muneue SUWA <m_suwa@s.okayama-u.ac.jp>
2022-04-22 01:50:25 +0900

    fix: pytestが動作するようにした

--- tests/test_hash_verifier.py
+++ tests/test_hash_verifier.py
@@ -1,10 +1,14 @@
 from pathlib import Path
-
 import pytest
-from hash_verifier import hash_verifier
+
+from hash_verifier import hash_verifier, __version__
 
 PROJ_DIRNAME = Path(__file__).resolve().parent
 TARGET_FILENAME = PROJ_DIRNAME / "sample_target_file.png"
+
+
+def test_version():
+    assert __version__ == '3.1.0'
 
 
 def test_all_hash_files():
